3ae5048ecf49031a8e4d13e49bd8638cf60f2c07
Miklós Koren <miklos.koren@gmail.com>
2024-10-28 18:56:37 +0100

    Add templates to pipx package

--- src/npr/cli.py
+++ src/npr/cli.py
@@ -7,8 +7,33 @@ from rich.panel import Panel
 import jinja2
 from dataclasses import dataclass, field
 from typing import List
+from importlib import resources
+from jinja2 import Environment, FileSystemLoader
 
-console = Console()
+def get_template_dir() -> Path:
+    """Get the template directory path that works both in development and after installation."""
+    try:
+        # Python 3.9+, without using context manager
+        template_dir = resources.files('npr').joinpath('templates')
+        return Path(template_dir)
+    except AttributeError:
+        # Fallback for older Python versions
+        import pkg_resources
+        return Path(pkg_resources.resource_filename('npr', 'templates'))
+
+def create_jinja_env() -> Environment:
+    """Create a Jinja environment with the correct template loader."""
+    template_dir = get_template_dir()
+    return Environment(
+        loader=FileSystemLoader(str(template_dir)),
+        autoescape=True
+    )
+
+def render_template(template_name: str, **kwargs) -> str:
+    """Render a template with the given variables."""
+    env = create_jinja_env()
+    template = env.get_template(template_name)
+    return template.render(**kwargs)
 
 # dataclass of a Tool object, with name, description, list of paths to create and a list of gitignore lines
 # there is also an f string that uses variables from the class, which will be run in the shell upon initializing the tools
@@ -25,25 +50,22 @@ class Project:
     name: str
     tools: List[Tool]
 
-# list of Tool objects
-project = Tool([], 'mkdir -p {name}', inside_folder=False)
-git = Tool([], 'git init', inside_folder=True)
-bead = Tool(['input/', 'temp/'], 'bead new {name}', inside_folder=False)
-poetry = Tool(['poetry.lock'], 'poetry init', inside_folder=True)
-julia = Tool(['Manifest.toml'], 'julia --project=. -e "using Pkg; Pkg.instantiate()"', inside_folder=True)
+def init_project(project):
+    gitignore = []
+    for tool in project.tools:
+        gitignore.extend(tool.gitignore)
 
-def add_tool(project, tool):
-    '''
-    Add a tool to the project. If `inside_folder`, change to `name` and then run the `init` command of the tool. Add the list in `gitignore` to `name/.gitignore`.
-    '''
-    project.tools.append(tool)
-    commands = []
-    if tool.inside_folder:
-        commands.append(f'cd {project.name}')
-    commands.append(tool.init)
-    subprocess.run(' && '.join(commands), check=True)
-    with open(f'{project.name}/.gitignore', 'a') as f:
-        f.write('\n'.join(tool.gitignore))
+    for tool in project.tools:
+        if tool.inside_folder:
+            subprocess.run(tool.init.format(name=project.name).split(), cwd=project.name, check=True)
+        else:
+            subprocess.run(tool.init.format(name=project.name).split(), check=True)
+
+    with open(f'{project.name}/.gitignore', 'w') as f:
+        f.write('\n'.join(gitignore))
+
+
+console = Console()
 
 def print_welcome():
     """Display a welcome message."""
@@ -55,11 +77,21 @@ def print_welcome():
     console.print(welcome)
     console.print()
 
+# list of Tool objects
+git = Tool([], 'git init', inside_folder=True)
+bead = Tool(['input/', 'temp/'], 'bead new {name}', inside_folder=False)
+poetry = Tool(['poetry.lock'], 'poetry init', inside_folder=True)
+julia = Tool(['Manifest.toml'], 'julia --project=. -e "using Pkg; Pkg.instantiate()"', inside_folder=True)
+
+
 @click.command()
 @click.option('--path', '-p', default='.',
               help='Path where project will be created',
               type=click.Path())
 def main(path: str):
+    print(
+        get_template_dir()
+    )
     """Create a new project directory and initialize git."""
     try:
         print_welcome()
@@ -73,9 +105,8 @@ def main(path: str):
         # Create project
         # Convert path to Path object
         path = Path(path)
-        new_project = Project(path, name, [project, git, bead])
-        for tool in new_project.tools:
-            add_tool(new_project, tool)
+        new_project = Project(path, name, [bead, git])
+        init_project(new_project)
 
         # Success message
         console.print(f"\n[bold green]✓[/bold green] Project created at [cyan]{path / name}[/cyan]")
